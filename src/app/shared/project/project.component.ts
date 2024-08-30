bcd1b75c5ae608596bdc095622f80e5c302eeb36
Mahalakshmi-afk <mahalakshmi26110@gmail.com>
2024-08-30 16:40:17 +0530

    second commit

--- src/app/shared/project/project.component.ts
+++ src/app/shared/project/project.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './project.component.css'
 })
 export class ProjectComponent {
+  filterProjects(filter: string): void {
+    const portfolioItems = document.querySelectorAll('.portfolio-item');
+
+    portfolioItems.forEach(item => {
+      if (filter === '*' || item.classList.contains(filter)) {
+        (item as HTMLElement).style.display = 'block';
+      } else {
+        (item as HTMLElement).style.display = 'none';
+      }
+    });
+  }
 
 }
